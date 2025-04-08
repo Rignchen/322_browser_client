3ff732988681039c87443382878e6c0ffa92bd5d
Rignchen <rinchendemort@gmail.com>
2025-04-08 10:07:57 +0200

    feat(walk): addapt type to match new backend type

--- src/types/walk.ts
+++ src/types/walk.ts
@@ -11,21 +11,29 @@ export default class {
 	elevation_gain_m: number = 0;
 	terrain: string = '';
 	accessibility: string[] = [];
+	difficulty: string = '';
 
-	fromID(id: number) {
-		fetch(`${env.API_URL}/walks/${id}`)
-			.then((response) => response.json())
-			.then((data) => {
+	fromObject(data: any) {
 				this.id = data.id;
 				this.name = data.name;
 				this.image_url = data.image_url;
-				this.map_url = "https://www.google.com/maps/embed?" + data.map_url;
+				this.map_url = data.map_url;
 				this.description = data.description;
 				this.duration = data.duration;
 				this.distance_km = data.distance_km;
 				this.elevation_gain_m = data.elevation_gain_m;
 				this.terrain = data.terrain;
 				this.accessibility = data.accessibility;
+				this.difficulty = data.difficulty;
+	}
+
+	fromID(id: number) {
+		fetch(`${env.API_URL}/walks?id=${id}`)
+			.then((response) => response.json())
+			.then((data) => {
+				data = data[0];
+				data.map_url = "https://www.google.com/maps/embed?" + data.map_url;
+				this.fromObject(data);
 			});
 	}
 }
