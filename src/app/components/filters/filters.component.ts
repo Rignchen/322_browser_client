97e5312985adc417b5c10d18b27f3406fb41aa90
Rignchen <rinchendemort@gmail.com>
2025-04-09 15:31:03 +0200

    feat(filter): display checkbox and name

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,37 +1,42 @@
 import env from '#types/env.js';
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-filters',
-	imports: [],
+	imports: [ NgFor ],
 	templateUrl: './filters.component.html',
 	styleUrl: './filters.component.scss'
 })
 export class FiltersComponent implements OnInit {
-	json = JSON;
-	filters = {
-		accessibilities: [
-			"Children",
-			"Dog-friendly",
-			"Cycling",
-			"Wealchair",
-			"Accessible in winter"
-		],
-		difficulties: [
-			"Beginner",
-			"Intermediate",
-			"Advanced"
-		],
-		terrains: [
-			"Forest",
-			"Mountain",
-			"Lake"
-		]
-	}
+	accessibilities = [
+		"Children",
+		"Dog-friendly",
+		"Cycling",
+		"Wealchair",
+		"Accessible in winter"
+	];
+	difficulties = [
+		"Beginner",
+		"Intermediate",
+		"Advanced"
+	];
+	terrains = [
+		"Forest",
+		"Mountain",
+		"Lake"
+	];
 
 	ngOnInit(): void {
 		fetch(`${env.API_URL}/filters`)
 			.then(response => response.json())
-			.then(data => this.filters = data);
+			.then(data => {
+        if (!data) {
+          throw new Error('No data received');
+        }
+				this.accessibilities = data.accessibilities;
+				this.difficulties = data.difficulties;
+				this.terrains = data.terrains;
+			})
 	}
 }
