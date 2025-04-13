ebb77c088108498d1d5efac627c37a36dc7c8724
Rignchen <rinchendemort@gmail.com>
2025-04-13 13:23:38 +0200

    feat(search): display data

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,11 +1,12 @@
 import { FiltersComponent } from '#components/filters/filters.component.js';
 import Walk from '#types/walk.js';
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-search',
-	imports: [ FiltersComponent ],
+	imports: [ NgFor, FiltersComponent, RouterLink ],
 	templateUrl: './search.component.html',
 	styleUrl: './search.component.scss'
 })
