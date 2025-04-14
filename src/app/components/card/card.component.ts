73cec082e06eca9832b3f5e1636679543edf0cdb
Abigaëlle Cornet <abigaelle.cornet@nagra.com>
2025-04-14 14:19:40 +0200

    Ajout du composant Card dans le dossier composant

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'; // ✅ Importation du module MatCard
+import { MatIconModule } from '@angular/material/icon'; // Importation du module MatIcon
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  standalone: true, // Ajout de cette option pour utiliser les imports dans le composant
+  imports: [MatCardModule, MatIconModule] // Ajout des modules ici pour ce composant
+  
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() content: string = '';
+  @Input() image: string = '';
+  @Input() distance: number = 0;
+  @Input() duration: number = 0;
+  @Input() isExpanded: boolean = false;
+
+
+}
