691530ba06166766811981ce313ed2cdbe8e3929
d-aaf <fabdulazeez@vaudoise.ch>
2025-04-25 15:41:21 +0200

    MatDividerModule removed

--- src/app/components/error404/error404.component.ts
+++ src/app/components/error404/error404.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon' 
 import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
 import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-error404',
-  imports: [MatIconModule, MatButtonModule, MatDividerModule, RouterLink],
+  imports: [MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './error404.component.html',
   styleUrl: './error404.component.scss'
 })
