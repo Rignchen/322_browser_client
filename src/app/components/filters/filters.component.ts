a3b4b5c2918b3cdc0b7b6e1102817f49d1a6351d
Rignchen <rinchendemort@gmail.com>
2025-04-28 13:57:03 +0200

    feat(filter): update url with duration

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -13,6 +13,9 @@ import {MatSliderModule} from '@angular/material/slider';
 export class FiltersComponent implements OnInit {
 	constructor(private route: ActivatedRoute, private router: Router) {}
 
+	activeFilters: { [key: string]: string[] } = {};
+	params: { [key: string]: string } = {};
+
 	accessibilities = [
 		"Children",
 		"Dog-friendly",
@@ -31,9 +34,10 @@ export class FiltersComponent implements OnInit {
 		"Lake"
 	];
 	durations: [number, number] = [1, 6];
-
-	activeFilters: { [key: string]: string[] } = {};
-	params: { [key: string]: string } = {};
+	durationsFilter: [number, number] = [
+		this.params['min'] ? Number(this.params['min']) : this.durations[0],
+		this.params['max'] ? Number(this.params['max']) : this.durations[1],
+	];
 
 	ngOnInit(): void {
 		this.route.queryParams.subscribe(params => this.params = params);
@@ -49,7 +53,19 @@ export class FiltersComponent implements OnInit {
 				this.difficulties = data.difficulties;
 				this.terrains = data.terrains;
 				this.durations = [data.durations['min'], data.durations['max']];
+				this.durationsFilter = [
+					this.params['min'] ? Number(this.params['min']) : this.durations[0],
+					this.params['max'] ? Number(this.params['max']) : this.durations[1],
+				];
 			})
+	}
+
+	onDurationChange(event: Event, index: number) {
+		const slider = event.target as HTMLInputElement;
+		this.durationsFilter[index] = Number(slider.value);
+
+		this.updateParamsDuration(index);
+		this.router.navigate(['search'], { queryParams: this.params, replaceUrl: true });
 	}
 
 	onCheckboxChange(event: Event, filterCategory: string, filterName: string) {
@@ -72,11 +88,11 @@ export class FiltersComponent implements OnInit {
 		this.activeFilters[filterCategory] = filterValues;
 
 		// Log the active filters
-		this.updateParams()
+		this.updateParamsFilters()
 		this.router.navigate(['search'], { queryParams: this.params, replaceUrl: true });
 	}
 
-	updateParams() {
+	updateParamsFilters() {
 		console.log("Updating params with active filters:", this.activeFilters);
 		let filters = new Map<string, string>();
 		for (const [key, values] of Object.entries(this.activeFilters)) {
@@ -91,5 +107,16 @@ export class FiltersComponent implements OnInit {
 				delete this.params[key];
 			}
 		}
+	}
+
+	updateParamsDuration(index: number) {
+		console.log("Updating params with duration:", this.durationsFilter);
+		const key = ['min', 'max'][index];
+		const temp: { [key: string]: string } = {};
+		temp[key] = this.durationsFilter[index].toString();
+		this.params = {
+			...this.params,
+			...temp
+		};
 	}
 }
