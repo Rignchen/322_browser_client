737e43630eddba2fc48848a145b0c66e2df3c930
Abigaëlle Cornet <abigaelle.cornet@nagra.com>
2025-04-28 08:29:15 +0200

    fix: revert bad stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { WalkDetailComponent } from './pages/walk-detail/walk-detail.component';
 import { SearchComponent } from './pages/search/search.component';
-import { TempComponent } from './temp/temp.component';
 
 export const routes: Routes = [
-  {path: "temp", component: TempComponent},
-  {path: "walk/:id n   v       ", component: WalkDetailComponent},
+  {path: "walk/:id", component: WalkDetailComponent},
   {path: "search", component: SearchComponent}
 ];
