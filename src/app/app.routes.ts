88f77f0b92a6b31b43db81365b9dab5a673c3589
Abigaëlle Cornet <abigaelle.cornet@nagra.com>
2025-04-28 08:11:49 +0200

    feat: add card component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WalkDetailComponent } from './pages/walk-detail/walk-detail.component';
 import { SearchComponent } from './pages/search/search.component';
+import { TempComponent } from './temp/temp.component';
 
 export const routes: Routes = [
-  {path: "walk/:id", component: WalkDetailComponent},
+  {path: "temp", component: TempComponent},
+  {path: "walk/:id n   v       ", component: WalkDetailComponent},
   {path: "search", component: SearchComponent}
 ];
---
+++ src/app/temp/temp.component.ts
@@ -0,0 +1,12 @@
+import { CardComponent } from '#components/card/card.component.js';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temp',
+  imports: [CardComponent],
+  templateUrl: './temp.component.html',
+  styleUrl: './temp.component.scss'
+})
+export class TempComponent {
+
+}
