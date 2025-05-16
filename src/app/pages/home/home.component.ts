45eaeb7166aafcd49c446e81a77705e376774330
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-05-16 21:52:59 +0200

    feat: implement home component with featured walks and categories

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { CardComponent } from '#components/card/card.component.js';
+import { MatButtonModule } from '@angular/material/button';
+import Walk from '#types/walk.js';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, MatIconModule, CardComponent, MatButtonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  categories = [
+    {
+      name: 'Montagne',
+      icon: 'terrain',
+      route: '/search',
+      params: { t: 'Mountain' },
+    },
+    {
+      name: 'Été',
+      icon: 'wb_sunny',
+      route: '/search',
+      params: { s: 'summer' },
+    },
+    {
+      name: 'Court (< 2h)',
+      icon: 'schedule',
+      route: '/search',
+      params: { max: '2' },
+    },
+    {
+      name: 'Débutant',
+      icon: 'accessibility_new',
+      route: '/search',
+      params: { d: 'Beginner' },
+    },
+  ];
+
+  featuredWalks: Walk[] = [];
+  magazineTitle: string = 'Découvrez Passer Simple';
+  magazineDescription: string =
+    "Le magazine qui fait revivre l'histoire romande";
+
+  constructor(public router: Router) {}
+
+  ngOnInit(): void {
+    Walk.fetchFeatured().then((walks) => {
+      this.featuredWalks = walks;
+    });
+  }
+
+  navigateToCategory(category: any): void {
+    this.router.navigate([category.route], { queryParams: category.params });
+  }
+
+  navigateToWalk(walkId: number): void {
+    this.router.navigate(['/walk', walkId]);
+  }
+}
