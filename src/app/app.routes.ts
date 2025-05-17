5a49b60bee7e69bc3b9bddb0dbf99218c983282d
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-05-17 16:12:57 +0200

    Feature/home page (#14)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { WalkDetailComponent } from './pages/walk-detail/walk-detail.component';
 import { SearchComponent } from './pages/search/search.component';
 import { Error404Component } from './components/error404/error404.component';
 import { Error500Component } from './components/error500/error500.component';
+import { HomeComponent } from './pages/home/home.component';
 import { PasserSimpleComponent } from './pages/passer-simple/passer-simple.component';
 
-
 export const routes: Routes = [
+    {path: '', component: HomeComponent},
     {path: "walk/:id", component: WalkDetailComponent},
     {path: "passer-simple", component: PasserSimpleComponent},
     {path: "search", component: SearchComponent},
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { CardComponent } from '#components/card/card.component.js';
+import { MatButtonModule } from '@angular/material/button';
+import Walk from '#types/walk.js';
+import Category from '#types/category.js';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, MatIconModule, CardComponent, MatButtonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  categories: Category[] = [];
+  featuredWalks: Walk[] = [];
+
+  constructor(public router: Router) {
+    // Initialize categories with router
+    this.categories = [
+      new Category('Montagne', 'terrain', '/search', { t: 'Mountain' }),
+      new Category('Été', 'wb_sunny', '/search', { a: 'Summer' }),
+      new Category('Court (< 2h)', 'schedule', '/search', { max: '2' }),
+      new Category('Débutant', 'accessibility_new', '/search', {
+        d: 'Beginner',
+      }),
+    ];
+
+    // Set router for each category
+    this.categories.forEach((category) => category.setRouter(this.router));
+  }
+
+  ngOnInit(): void {
+    Walk.fetchFeatured().then((walks) => {
+      this.featuredWalks = walks;
+    });
+  }
+
+  navigateToWalk(walkId: number): void {
+    this.router.navigate(['/walk', walkId]);
+  }
+}
