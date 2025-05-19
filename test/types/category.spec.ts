7cf20d92ad5ec588e31d59337fbc64113c7828f9
Rignchen <rinchendemort@gmail.com>
2025-05-20 00:05:09 +0200

    test: fix category redirection test to ensure the mocked function is run

--- test/types/category.spec.ts
+++ test/types/category.spec.ts
@@ -2,25 +2,34 @@ import { Router } from '@angular/router';
 import { it, expect } from 'vitest';
 import Category from '#types/category.js';
 
+class MockRouter {
+	navigate_route: string[] = [];
+	navigate_params: Record<string, string> = {};
+
+	navigate(route: string[], options: { queryParams: Record<string, string> }): Promise<Boolean> {
+		this.navigate_route = route;
+		this.navigate_params = options.queryParams;
+		return new Promise((resolve) => {
+			resolve(true);
+		});
+	}
+};
+
 it('should redirect properly', () => {
-  const expected = {
-    name: 'Category Name',
-    icon: 'category-icon',
-    route: 'category-route',
-    params: {
-      param1: 'category-param1',
-      param2: 'category-param2',
-    }
-  };
+	const expected = {
+		name: 'Category Name',
+		icon: 'category-icon',
+		route: 'category-route',
+		params: {
+			param1: 'category-param1',
+			param2: 'category-param2',
+		}
+	};
 
-  const router = {
-    navigate: (route: string[], options: { queryParams: Record<string, string> }) => {
-      expect(route).toEqual([expected.route]);
-      expect(options.queryParams).toEqual(expected.params);
-    }
-  };
-
-  const category = new Category(expected.name, expected.icon, expected.route, expected.params);
-  category.setRouter(router as unknown as Router); // Apparently this makes both typescript and angular happy
-  category.goToCategory();
+	const category = new Category(expected.name, expected.icon, expected.route, expected.params);
+	const router = new MockRouter();
+	category.setRouter(router as unknown as Router); // Apparently this makes both typescript and angular happy
+	category.goToCategory();
+	expect(router.navigate_route).toEqual([expected.route]);
+	expect(router.navigate_params).toEqual(expected.params);
 });
