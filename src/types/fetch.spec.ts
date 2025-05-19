162fc85918ddd436791a1e0aca7ffe36db2f28dc
Rignchen <rinchendemort@gmail.com>
2025-05-19 17:39:16 +0200

    test: ensure formatting of filters to target the backend

---
+++ src/types/fetch.spec.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect } from 'vitest';
+import fetcher from './fetch'
+
+describe('formatFilters', () => {
+	it('should be empty if all parameters are null', () => {
+		const result = fetcher.formatFilters(null, [], [], [null, null]);
+		expect(result).toBe('');
+	});
+	it('should format name properly', () => {
+		const result = fetcher.formatFilters('test', [], [], [null, null]);
+		expect(result).toBe('?name_like=test');
+	});
+	describe('should format difficulty properly', () => {
+		it('with one value', () => {
+			const result = fetcher.formatFilters(null, ['Beginner'], [], [null, null]);
+			expect(result).toBe('?difficulty[]=Beginner');
+		});
+		it('with multiple values', () => {
+			const result = fetcher.formatFilters(null, ['Beginner', 'Intermediate'], [], [null, null]);
+			expect(result).toBe('?difficulty[]=Beginner&difficulty[]=Intermediate');
+		});
+	});
+	describe('should format terrain properly', () => {
+		it('with one value', () => {
+			const result = fetcher.formatFilters(null, [], ['Mountain'], [null, null]);
+			expect(result).toBe('?terrain[]=Mountain');
+		});
+		it('with multiple values', () => {
+			const result = fetcher.formatFilters(null, [], ['Mountain', 'Forest'], [null, null]);
+			expect(result).toBe('?terrain[]=Mountain&terrain[]=Forest');
+		});
+	});
+	describe('should format duration properly', () => {
+		it('with only min value', () => {
+			const result = fetcher.formatFilters(null, [], [], [42, null]);
+			expect(result).toBe('?duration_gte=42');
+		});
+		it('with only max value', () => {
+			const result = fetcher.formatFilters(null, [], [], [null, 69]);
+			expect(result).toBe('?duration_lte=69');
+		});
+		it('with both values', () => {
+			const result = fetcher.formatFilters(null, [], [], [42, 69]);
+			expect(result).toBe('?duration_gte=42&duration_lte=69');
+		});
+	});
+	it('should format all parameters together', () => {
+		const result = fetcher.formatFilters('test', ['Beginner', 'Intermediate'], ['Mountain', 'Forest'], [42, 69]);
+		expect(result).toBe('?name_like=test&difficulty[]=Beginner&difficulty[]=Intermediate&terrain[]=Mountain&terrain[]=Forest&duration_gte=42&duration_lte=69');
+	});
+});
