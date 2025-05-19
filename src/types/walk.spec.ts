14cc3f6f6c34914342218e4b1d882009086b5bf1
Rignchen <rinchendemort@gmail.com>
2025-05-19 19:59:39 +0200

    test: ensure the creation of walk type

--- src/types/walk.spec.ts
+++ src/types/walk.spec.ts
@@ -1,6 +1,50 @@
 import { describe, it, expect } from 'vitest';
 import Walk from './walk';
 
+describe('instantiate a walk', () => {
+	it('should create a walk with empty values', () => {
+		const walk = new Walk();
+		const expected = [
+			['id', 0],
+			['name', ''],
+			['image_url', ''],
+			['description', ''],
+			['duration', 0],
+			['distance_km', 0],
+			['elevation_gain_m', 0],
+			['terrain', ''],
+			['path', []],
+			['accessibility', []],
+			['difficulty', ''],
+			['map_link', ''],
+		];
+		expect(walk).toBeInstanceOf(Walk);
+		expect(Object.entries(walk)).toEqual(expected);
+	});
+	it('Should correctly fill the walk with data', () => {
+		const data = {
+			id: 1,
+			name: 'Walk 1',
+			image_url: 'https://example.com/image.jpg',
+			description: 'A beautiful walk',
+			duration: 120,
+			distance_km: 5,
+			elevation_gain_m: 200,
+			terrain: 'mountain',
+			path: [
+				{ lat: 1.0, lng: 2.0 },
+				{ lat: 3.0, lng: 4.0 },
+			],
+			accessibility: ['A', 'B'],
+			difficulty: 'Beginner',
+			map_link: 'https://example.com/map',
+		};
+		const walk = (new Walk).fromObject(data);
+		expect(walk).toBeInstanceOf(Walk);
+		expect(Object.entries(walk)).toEqual(Object.entries(data));
+	});
+});
+
 describe('filter walks on accessibility', () => {
 	it('should not change the data if no accessibility is selected', () => {
 		const data = [
